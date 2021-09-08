f2c8bd11d3b77a7419804f0e015e5c1903453c96
saeed-wahab <salamwahab16@gmail.com>
2021-09-08 11:08:35 +0000

    created action types

--- src/global-state/action.js
+++ src/global-state/action.js
@@ -1,68 +1,27 @@
+import { 
+  FIND_ADDRESS,
+  FOUND_ADDRESS,
+  ADDRESS_NOT_FOUND,
+  FIND_GPS_COORDINATE,
+  FOUND_GPS_COORDINATE,
+  GPS_COORDINATES_NOT_FOUND,
+  SET_FIBRE_AVAILABILITY,
+  DISABLE_NOTIFICATION,
+  MAP_CLICK
+} from "./actionTypes"
+
 export function translateGPSAddress(data){
   return(
   {
-    type:"locate-ghpostAddress",
+    type:FIND_ADDRESS,
     payload: data
   })
 }
 
-export function gpsLocate(){
-  return(
-    {
-      type:"locate-gps"
-    }
-  )
-}
-
-export function loadedGeojson(){
-  return(
-    {
-      type:"geojson-loaded"
-    }
-  )
-}
-export function mapWasClicked(data){
-  return(
-    {
-      type:"map-click",
-      payload:data
-    }
-  )
-}
-export function plotGPScoords({latitude,longitude}){
-  return(
-    {
-    type:"gps-found",
-    payload:{
-      lat:latitude,
-      lng:longitude
-    }
-  })
-
-}
-
-export function setAvailability(data){
-  return(
-    {
-      type:"fibre-availability",
-      payload:data
-    }
-  )
-
-}
-
-export function setGPSFailed(message){
-  return(
-    {
-      type:"gps-not-found",
-      payload:message
-    }
-  )
-}
 export function plotGhanaGPS(lat,lng){
   return(
     {
-      type:"address-found",
+      type:FOUND_ADDRESS,
       payload:{
         lat:lat,
         lng:lng
@@ -75,12 +34,65 @@ export function plotGhanaGPS(lat,lng){
 export function addressNotFound(){
   return(
     {
-      type:"address-not-found"
+      type:ADDRESS_NOT_FOUND,
     }
   )
 }
+
+export function gpsLocate(){
+  return(
+    {
+      type:FIND_GPS_COORDINATE
+    }
+  )
+}
+
+export function plotGPScoords({latitude,longitude}){
+  return(
+    {
+    type:FOUND_GPS_COORDINATE,
+    payload:{
+      lat:latitude,
+      lng:longitude
+    }
+  })
+
+}
+
+export function setGPSFailed(message){
+  return(
+    {
+      type:GPS_COORDINATES_NOT_FOUND,
+      payload:message
+    }
+  )
+}
+
+
+
+export function setAvailability(data){
+  return(
+    {
+      type:SET_FIBRE_AVAILABILITY,
+      payload:data
+    }
+  )
+
+}
+
+export function mapWasClicked(data){
+  return(
+    {
+      type:MAP_CLICK,
+      payload:data
+    }
+  )
+}
+
+
+
 export function disableNotification(){
   return({
-    type:"disable-notification"
+    type:DISABLE_NOTIFICATION
   })
 }
