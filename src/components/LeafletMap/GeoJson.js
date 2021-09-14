d31de3d9d55b9648a815778e9b846863fd8b83e1
saeed-wahab <salamwahab16@gmail.com>
2021-09-14 17:22:02 +0000

    restricted GeoJson.js usage outside MapContainer

--- src/components/LeafletMap/GeoJson.js
+++ src/components/LeafletMap/GeoJson.js
@@ -5,10 +5,14 @@ import {filterGeoJson } from "../../util";
 
 
 function GeoJson({data,color="#ffc423", opacity=0.4}){
-    const context  = useLeafletContext(); 
+    const context  = useLeafletContext();
+     
+    if(!context.map){
+        throw new Error("GeoJson component should be within a MapContainer component");
+    }
 
     useEffect(()=>{
-        const container = context.layerContainer || context.map;
+        const container = context.map;
 
         const geo_json = L.geoJSON(data, {
             style: { color: color, opacity: opacity },
