85ddc5416674de1fbdb8b918a4b8404e681e07fc
saeed-wahab <salamwahab16@gmail.com>
2021-09-26 23:40:59 +0000

    patial update to suit refactored code

---
+++ src/api/ghpostAddress.js
@@ -0,0 +1,47 @@
+/* Ghana Post address engineering was done by sperixlabs.org */
+
+const URL = "https://ghanapostgps.sperixlabs.org";
+const urlencoded = new URLSearchParams();
+const methodAndHeaders = {
+  'method': 'POST',
+  'headers': {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+async function getAddress(address,successful, failed){  
+  
+  urlencoded.set("address", address)
+  let options = {
+    ...methodAndHeaders,
+    body:urlencoded.toString(),
+  };
+
+  try{
+    const json = await fetch(URL, options).then(data=>data.json())
+    successful(json);
+  }catch{
+    failed();
+  }finally{
+      //stop progress bar here
+  }
+  
+
+}
+
+function findAddressWithCoords(coords) {
+  urlencoded.set("lat", coords.latitude.toString());
+  urlencoded.set("long", coords.longitude.toString());
+  let options = {
+    ...methodAndHeaders,
+    body:urlencoded.toString(),
+  };
+
+
+  return (fetch("https://ghanapostgps.sperixlabs.org/get-address", options )
+  .then(response => response.json()))
+
+  
+}
+
+export { findAddressWithCoords };
+export default getAddress;
---
+++ src/api/ghpostAddress.test.js
@@ -0,0 +1,60 @@
+import getAddress,{ findAddressWithCoords } from "./ghpostAddress";
+
+describe("Test for Ghana Post Address Api",()=>{
+
+    it("testing with Gh adddress",()=>{
+        return getAddress("AK-484-9321",
+            (res)=>{
+                expect(res.found).toBeTruthy();
+                expect(res.data.Table[0].Area).toBe(result.Table[0].Area);
+                expect(res.data.Table[0].CenterLatitude).toBe(result.Table[0].CenterLatitude);
+                expect(res.data.Table[0].CenterLongitude).toBe(result.Table[0].CenterLongitude)
+        
+            }
+        ,
+        ()=>{
+            console.log("error")
+        })
+    
+    });
+
+    it("testing with coordinates", ()=>{
+        const latitude = result.Table[0].CenterLatitude;
+        const longitude = result.Table[0].CenterLongitude;
+        
+        return findAddressWithCoords({latitude,longitude}).then(
+            (res)=>{
+                expect(res.found).toBeTruthy();
+                expect(res.data.Table[0].Area).toBe(result.Table[0].Area);
+                expect(res.data.Table[0].GPSName).toBe(result.Table[0].GPSName);
+            }
+        )
+    });
+
+
+})
+
+
+const result={
+    "Table": [
+        {
+            "Area": "NEW KAGYASI",
+            "CenterLatitude": 6.650080145273592,
+            "CenterLongitude": -1.648700346667856,
+            "District": "Kumasi",
+            "EastLat": 6.65005768739201,
+            "EastLong": -1.6486780409076,
+            "GPSName": "AK4849321",
+            "NorthLat": 6.65010262239948,
+            "NorthLong": -1.6487229566718,
+            "PostCode": "AK484",
+            "Region": "Ashanti",
+            "SouthLat": 6.65005768739201,
+            "SouthLong": -1.6487229566718,
+            "Street": "Kumasi, Ashanti, GHA",
+            "WestLat": 6.65010262239948,
+            "WestLong": -1.6486780409076
+        }
+    ],
+    "found": true
+}
