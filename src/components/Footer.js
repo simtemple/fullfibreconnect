29bd84d035f22743b607d537188ad6e4151d62c8
Simon Ndulagwa <stndulagwa@gmail.com>
2024-09-22 19:19:00 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer=()=>{
   return(
     <div className="has-text-centered disclaimer">
       <p>&copy;2021 This is an app by <a href="https://twitter.com/io_saeed"> Saeed Wahab</a> </p>
-      <p>Disclaimer: This is not official MTN fibre website</p>
+      <p>Disclaimer: This is official Full fibre website</p>
     </div>
   )
 }
